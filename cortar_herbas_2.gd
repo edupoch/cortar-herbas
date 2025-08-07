d2c02524637bfbd6ea9325ff83f3472a2074cf66
edupoch <edupoch@gmail.com>
2025-08-07 10:01:08 +0200

    Segunda proba con elementos máis parametrizables

--- cortar_herbas_2.gd
+++ cortar_herbas_2.gd
@@ -1,22 +1,22 @@
 extends Node2D
+
 @onready var timer = $Timer
 @onready var segundos = $Segundos
 
-var initialPosition
+var barra
+var herbas
 
 var game_over = false
-
-var herbas
-var limite_tempo = 20
 
 var contador_herbas_cortadas = 0
 var contador_tempo = 0
 
+var limite_tempo = 20
+
 func _get_random_wait_time():
-	return randf_range(0.500, 4.0)
+	return randf_range(0.500, 4.0)	
 	
 func _ready():
-	initialPosition = $Herba.position
 	timer = $Timer
 	timer.connect("timeout", _on_timer_timeout)
 	timer.wait_time = _get_random_wait_time()
@@ -25,39 +25,39 @@ func _ready():
 	segundos = $Segundos
 	segundos.connect("timeout", _on_segundos_timeout)	
 	segundos.start()
-
+	
 	herbas = []
 	contador_tempo = limite_tempo
+	barra = $VBoxContainer/HBoxContainer/Barra;
 	
-	_actualiza_contador_tempo()
-
 func _on_timer_timeout():
 	if !game_over:
 		# Borramos as herbas que saen da pantalla
 		for herba in herbas:
 			if herba.position.x < 0:
-				remove_child(herba)
+				barra.remove_child(herba)
 				herbas.erase(herba)
 
-		var herba = $Herba.duplicate()
-		herba.position = initialPosition
-		# Hacemos la herbe sea visible
+		var herba = $VBoxContainer/HBoxContainer/Barra/HerbaColorRect.duplicate()
+		herba.position.x = barra.position.x + barra.custom_minimum_size.x
+		# Hacemos que a herba sexa visible
 		herba.visible = true
-		add_child(herba)
+		barra.add_child(herba)
 		herbas.append(herba)
 		
 		timer.wait_time = _get_random_wait_time()
-	
+
 func _on_segundos_timeout():
 	if !game_over:
 		contador_tempo = contador_tempo - 1;
 		$Contador/TextoFallaches.visible = false
 		$Contador/TextoOk.visible = false
-		_actualiza_contador_tempo()	
+		_actualiza_contador_tempo()
 
+	
 func _actualiza_contador_tempo():
 	$Contador/ContadorTempo.text = str(contador_tempo)
-	
+
 func _process(_delta):
 	if !game_over:
 		if Input.is_action_just_pressed("ui_accept"):
@@ -65,11 +65,11 @@ func _process(_delta):
 			var huboCorte = false
 			
 			for herba in herbas:			
-				if herba.get_node("Area2D").has_overlapping_areas() and herba.get_node("Area2D").get_overlapping_areas().has($Tixeira.get_node("Area2D")):
+				if herba.get_node("Area2D").has_overlapping_areas() and herba.get_node("Area2D").get_overlapping_areas().has($VBoxContainer/HBoxContainer/Barra/Tixeira.get_node("Area2D")):
 					print("Cortada herba")
 					contador_herbas_cortadas = contador_herbas_cortadas + 1
 					$Contador/ContadorHerbasCortadas.text = str(contador_herbas_cortadas)
-					remove_child(herba)
+					barra.remove_child(herba)
 					herbas.erase(herba)
 					huboCorte = true
 					$Contador/TextoOk.visible = true
